e7fa32411d649c226b05840586dfa071e5f727c2
mihad-khadem <mihadkhadem@gmail.com>
2024-04-27 19:44:34 +0600

    app created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h2 className="">App</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Knowledge Cafe</h1>
+    </div>
+  );
+};
+
+export default Header;
